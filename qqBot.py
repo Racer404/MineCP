abd46b32bb1f7200f9a08294874992d955aacd28
racer404 <2238976035@qq.com>
2026-08-29 09:17:01 +0700

    20260829

--- qqBot.py
+++ qqBot.py
@@ -1,5 +1,6 @@
 import websocket
 import requests
+import threading
 import time
 import json
 import os
@@ -11,6 +12,8 @@ class Bot:
         self.access_token = None
         self.token_expiresAt = None
         self.token_safety_time_margin = 50.
+        self.agreed_heartbeat_interval = 60
+        self.heartbeat_payload_d = None
 
     def update_access_token(self):
         time_now = time.time()
@@ -36,13 +39,87 @@ class Bot:
         terminal = response.json()['url']
         return terminal
 
-    def listenToSocket(self, on_receive_method):
+    def listenToSocket(self, url):
 
-        on_receive_method()
+        def on_message(socket, message):
+            print(f"Received: {message}")
+            op_ = json.loads(message)['op']
 
-def wssMessage(ws):
-    print("received")
-    breakpoint()
+            if op_ == 10:
+                print("Handshake")
+                self.agreed_heartbeat_interval = int(json.loads(message)['d']['heartbeat_interval'] / 1000)
+                authentication_payload = {
+                    "op": 2,
+                    "d": {
+                        "token": f"QQBot {self.access_token}",
+                        "intents": 33554432,
+                        "shard": [0, 1]
+                    }
+                }
+                socket.send(json.dumps(authentication_payload))
+
+            elif op_ == 0:
+                print("Dispatch from server...")
+                t_ = json.loads(message)['t']
+
+                if t_ == 'READY':
+                    print("Handshake successfully")
+                    self.heartbeat_payload_d = "null"
+
+
+            elif op_ == 11:
+                print("pong")
+
+        def on_error(error):
+            print(f"Error: {error}")
+
+        def on_close(socket, close_status_code, close_msg):
+            print("Connection closed")
+
+        def on_open(socket):
+            print("Connected to server")
+            payload = {
+                "op": 2,
+                "d": {
+                    "token": f"QQBot {bot.access_token}",
+                    "intents": 513,
+                    "shard": [0, 4],
+                    "properties": {
+                        "$os": "linux",
+                        "$browser": "my_library",
+                        "$device": "my_library"
+                    }
+                }
+            }
+            ws.send(json.dumps(payload))
+
+        def heartbeat(socket):
+            while True:
+                print("heartbeat function")
+                self.agreed_heartbeat_interval = 10
+                time.sleep(self.agreed_heartbeat_interval)
+
+                if self.heartbeat_payload_d:
+                    heartbeat_payload = {
+                        "op": 1,
+                        "d": self.heartbeat_payload_d
+                    }
+                    socket.send(json.dumps(heartbeat_payload))
+                    print("ping")
+                    self.heartbeat_payload_d = None
+
+
+        ws = websocket.WebSocketApp(url)
+        ws.on_message = on_message
+
+        print("threading start")
+        threading.Thread(
+            target=heartbeat,
+            args=(ws,),
+            daemon=True
+        ).start()
+
+        ws.run_forever()
 
 secret = str(os.getenv('QQ_APP_SECRET'))
 bot = Bot('1905231300', secret)
@@ -50,44 +127,4 @@ bot = Bot('1905231300', secret)
 bot.update_access_token()
 wssTerminal = bot.getWebSocketTerminal()
 
-server_url = wssTerminal
-
-def on_message(ws, message):
-    print(f"Received: {message}")
-
-def on_error(ws, error):
-    print(f"Error: {error}")
-
-def on_close(ws, close_status_code, close_msg):
-    print("Connection closed")
-
-def on_open(ws):
-    print("Connected to server")
-    # Send a message once the connection is open
-    payload = {
-              "op": 2,
-              "d": {
-                "token": f"QQBot {bot.access_token}",
-                "intents": 513,
-                "shard": [0, 4],
-                "properties": {
-                  "$os": "linux",
-                  "$browser": "my_library",
-                  "$device": "my_library"
-                }
-              }
-            }
-    ws.send(json.dumps(payload))
-
-ws = websocket.WebSocketApp(
-        server_url,
-        on_open=on_open,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close
-    )
-
-# Run the connection perpetually with a 60-second ping interval
-ws.run_forever(ping_interval=60, ping_timeout=10)
-
-bot.listenToSocket(on_message)
+bot.listenToSocket(wssTerminal)
